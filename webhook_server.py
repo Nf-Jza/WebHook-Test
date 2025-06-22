775f262556d6407a3e78362929ae4700809206ee
Nf-Jza <josa241102@gmail.com>
2025-06-23 01:28:38 +0700

    test

--- webhook_server.py
+++ webhook_server.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])  # ← POST must be here!
+@app.route('/', methods=['POST'])  # ← POST must be here!
 def webhook():
     data = request.json
     print("Webhook received:", data)
